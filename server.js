d6cea41bc796ec5bcaf23466a40a72ca42874001
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-08-02 21:25:13 +0500

    server.js update

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
 
 // Start the server
 app.listen(port, () => {
