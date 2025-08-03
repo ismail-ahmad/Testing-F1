acaddfa1cf10084d7644e85dd8f371c0b06aba59
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-08-03 12:54:00 +0500

    added express backend

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 3000; // 👈 Use dynamic port
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 // Enable CORS for your frontend domain
 app.use((req, res, next) => {
@@ -22,6 +24,22 @@ app.get('/', (req, res) => {
 });
 app.get('/form', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
+
+app.post('/form', (req, res) => {
+  fetch('https://script.google.com/macros/s/AKfycbwjApE_Q2Svwxn9xEAWalis_tqGrPVIVWI5ERDy4pkqIAJiUih3V_TJdy2Ax46mJmIOcQ/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(req.body)
+  })
+  .then(res => res.text())
+  .then(data => {
+    console.log(data);
+    res.json({status: 'success'});
+  })
+  .catch(err => console.log(err))
 });
 
 // Start the server
